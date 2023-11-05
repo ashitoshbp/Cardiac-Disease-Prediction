fd629f672e76902fac0d22fb33254b34cb6c4c67
ashitoshbp <107021261+ashitoshbp@users.noreply.github.com>
2023-11-06 00:55:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 # Load the Random Forest CLassifier model
 filename = 'heart-disease-prediction-knn-model.pkl'
@@ -49,5 +50,6 @@ def predict():
         
 
 if __name__ == '__main__':
-	app.run(debug=True )
-
+	#app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)
